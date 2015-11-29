37cd044c3c0cdc043dd4d4377024d4b1c83f9a11
Tomas Krizek <tomas.krizek@mailbox.org>
2015-11-29 18:15:12 +0100

    thread list design finished

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,12 @@ var boardApp = angular.module('boardApp', []);
 
 boardApp.controller('ThreadListCtrl', function($scope, $http) {
   $http.get('http://private-76979-simplediscussionboard.apiary-mock.com/threads').success(function(data) {
-    $scope.threads = data['threads'];
+    var threads = data['threads'];
+
+    for (var i = threads.length - 1; i >= 0; i--) {
+      threads[i].created = Date.parse(threads[i].created);
+    };
+
+    $scope.threads = threads;
   });
 });
